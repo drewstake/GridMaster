764ac50733d00183014ab21134a0437ab9b7b253
drewstake <ajn5605@psu.edu>
2024-07-21 17:46:22 -0400

    Update Firebase configuration
    - Updated Firebase configuration for better integration and deployment on AWS Amplify.

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPQbWVfBesOcJd0dOevCQ1DLSbk_KvqI4",
-  authDomain: "gridmaster-f2eb1.firebaseapp.com",
-  projectId: "gridmaster-f2eb1",
-  storageBucket: "gridmaster-f2eb1.appspot.com",
-  messagingSenderId: "279147952459",
-  appId: "1:279147952459:web:b3bc712ffc782fe0dbd0ce",
-  measurementId: "G-XJCP6BE6NZ",
-  databaseURL: "https://gridmaster-f2eb1-default-rtdb.firebaseio.com/"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);
